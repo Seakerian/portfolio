a3ddf6b00351d64e39bebaa1c930de1f44dc2203
Marvin Dominguez <marvindom1998@gmail.com>
2024-08-01 07:52:12 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
+import { Route, Routes } from "react-router-dom";
+import MainContent from "./components/main-content";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
