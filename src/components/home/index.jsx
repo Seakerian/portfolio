5d24fdf473308cb6794517b37b236ff48eadaf13
Marvin Dominguez <marvindom1998@gmail.com>
2024-08-01 07:57:53 -0500

    styled home and main-content components

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,3 +1,25 @@
+import TerminalIcon from "@mui/icons-material/Terminal";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div
+      id="home"
+      className="relative flex items-center justify-center h-screen bg-cover bg-center bg-no-repeat bg-fixed aspect-auto"
+      style={{
+        backgroundImage:
+          "url(https://ithemes.com/wp-content/uploads/2019/08/What-is-Your-Website-Design-Process-Blog-Post-Feature-Image-36119-01-1536x805.png)",
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative text-center text-white z-10 px-4 md:px-8">
+        <h1 className="text-6xl md:text-7xl font-merriweather mb-2">
+          Hi, I'm Marvin
+        </h1>
+        <h2 className="text-4xl md:text-5xl font-manrope">
+          a Web Developer <TerminalIcon fontSize="large" />{" "}
+          <span className="font-semibold animate-blink">|</span>{" "}
+        </h2>
+      </div>
+    </div>
+  );
 }
--- src/components/main-content/index.jsx
+++ src/components/main-content/index.jsx
@@ -1,3 +1,21 @@
+import About from "../about";
+import Contact from "../contact";
+import Home from "../home";
+import Projects from "../projects";
+
 export default function MainContent() {
-  return <div></div>;
+  return (
+    <div className="relative min-h-screen">
+      <div className="absolute inset-0 bg-pattern bg-fixed bg-center bg-cover opacity-20 z-0 aspect-auto"></div>
+      <div className="relative z-10">
+        <Home />
+        <hr className="w-48 h-1 mx-auto my-4 border-0 rounded md:my-8 dark:bg-gray-600"></hr>
+        <About />
+        <hr className="w-48 h-1 mx-auto my-4 border-0 rounded md:my-8 dark:bg-gray-600"></hr>
+        <Projects />
+        <hr className="w-48 h-1 mx-auto my-4 border-0 rounded md:my-8 dark:bg-gray-600"></hr>
+        <Contact />
+      </div>
+    </div>
+  );
 }
